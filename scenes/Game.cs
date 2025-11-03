8bf65eea5fcc5fd79c3ae00f86ef0a22203d0ee1
Sean Kain <sean.kain.geo@gmail.com>
2025-11-02 19:18:00 -0800

    just getting repo started

---
+++ scenes/Game.cs
@@ -0,0 +1,40 @@
+using Godot;
+using System;
+using System.Collections.Generic;
+
+public partial class Game : Node2D
+{
+
+[Export]
+public double cooldown = 5;
+
+private double elapsed = 0;
+
+    [Export]
+    public PackedScene EnemyScene;
+
+	private List<Node2D> enemyNodes = new();
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		elapsed+=delta;
+		if(elapsed >= cooldown){
+			SpawnEnemy(new Vector2{X=0,Y=-100});
+			elapsed = 0;
+		}
+	}
+
+	private void SpawnEnemy(Vector2 pos){
+		var p = GD.Load<PackedScene>(EnemyScene.ResourcePath);
+        var enemy = p.Instantiate();
+        AddChild(enemy);
+        ((Node2D)enemy).GlobalPosition = pos;
+        enemyNodes.Add((Node2D)enemy);
+	}
+}
